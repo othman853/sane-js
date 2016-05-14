3d06b5cfbf2795649ce1eb750f9cab13b058b2ad
othman853 <anuar.yasser@gmail.com>
2016-05-14 11:18:28 -0300

    HttpServer listens to request instead of express, making socket work

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ var http = require('http');
 var express = require('express');
 var io = require('socket.io');
 
-var server = http.createServer();
-var app = express(server);
+var app = express();
+var server = http.createServer(app);
 var socketServer = io(server);
 
-app.use(express.static(__dirname + '/views'))
+app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs');
 
-app.listen(3000, () => console.log('Up on 3000'));
+app.get('/', (request, response) => response.render('home'));
+
+server.listen(3000, () => console.log('Up on 3000'));
 
 socketServer.on('connection', (client) => {
-  console.log('Server connected: ', client);
+  console.log('Server connected to the client.');
 });
-
-app.get('/', (request, response) => response.render('home'));
