aca678d45fb011d65e896a608da3406d8b1b95dd
othman853 <anuar.yasser@gmail.com>
2016-05-14 12:36:04 -0300

    Serving a babelified js as test

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var http = require('http');
 var express = require('express');
 var io = require('socket.io');
+var browserify = require('browserify-middleware');
+var babelify = require('babelify');
 
 var app = express();
 var server = http.createServer(app);
@@ -12,6 +14,16 @@ app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => response.render('home'));
+
+app.get('/js', browserify(`${__dirname}/public/js/`, {
+
+    extensions: ['.js'],
+
+    transform: [babelify.configure({
+        presets: ['es2015']
+    })]
+
+}));
 
 server.listen(3000, () => console.log('Up on 3000'));
 
