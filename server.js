7f7545c9356607202d4fbc88d3483822cff2d2cf
othman853 <anuar.yasser@gmail.com>
2016-05-14 00:32:50 -0300

    Adding simple view

--- server.js
+++ server.js
@@ -3,24 +3,18 @@
 var http = require('http');
 var express = require('express');
 var io = require('socket.io');
-var ioClient = require('socket.io-client');
 
 var server = http.createServer();
 var app = express(server);
 var socketServer = io(server);
 
+app.use(express.static(__dirname + '/views'))
+app.set('view engine', 'ejs');
+
 app.listen(3000, () => console.log('Up on 3000'));
 
 socketServer.on('connection', (client) => {
-
-  console.log('Client connected: ', client);
+  console.log('Server connected: ', client);
 });
 
-app.get('/', (request, response) => {
-
-  console.log('Io Client', ioClient);
-
-  response.send('Oi');
-});
-
-app.get('/js/socket.js');
+app.get('/', (request, response) => response.render('home'));
